9c65c8c1de4576fceb4249491aab85ef55b8f61d
The-Livrodjx <45701263+The-Livrodjx@users.noreply.github.com>
2021-03-09 03:36:35 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize")
 
-const connection = new Sequelize('ahegao', 'root', '@Python123', {
+const connection = new Sequelize('ahegao', 'root', '@MyPassWord', {
 
     host: "localhost",
     dialect: "mysql",
